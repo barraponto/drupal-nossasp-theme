dbd5956c57688f487544106f1d04fc8a56110e94
Capi Etheriel <barraponto@gmail.com>
2011-10-21 13:21:41 -0200

    corrigindo alguns errinhos de url de imagem e um bug bobo no js

--- js/nossasp.js
+++ js/nossasp.js
@@ -1,23 +1,23 @@
 /* função que mostra/esconde o box do filtro "Área de atuação" */
 function area_atuacao(id) {
-    var e = document.getElementById(id);
-    if(e.style.display=='block') {
-        e.style.display = 'none';
-        document.forms['views-exposed-form-nossasp-organizations-map-page-2'].submit();
-    } else {
-        e.style.display = 'block';
-    }
+  var e = document.getElementById(id);
+  if(e.style.display=='block') {
+    e.style.display = 'none';
+    document.forms['views-exposed-form-nossasp-organizations-map-page-2'].submit();
+  } else {
+    e.style.display = 'block';
+  }
 }
 
 WebFontConfig = {
-          google: { families: [ 'Abel::latin' ] }
-            };
-              (function() {
-                      var wf = document.createElement('script');
-                          wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-                                '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-                                    wf.type = 'text/javascript';
-                                        wf.async = 'true';
-                                            var s = document.getElementsByTagName('script')[0];
-                                                s.parentNode.insertBefore(wf, s);
-                                                  })(); </script>
+  google: { families: [ 'Abel::latin' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+  '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+wf.type = 'text/javascript';
+wf.async = 'true';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(wf, s);
+})();
