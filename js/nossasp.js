1dd945845f31dbe9bb9e59576867cfa81aa30438
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2011-10-21 02:14:10 -0200

    Novas alteracoes no layout, sombras, fontes, etc. É preciso configurar o módulo fontyourface para baixar a googlefont Arimo

--- js/nossasp.js
+++ js/nossasp.js
@@ -8,3 +8,16 @@ function area_atuacao(id) {
         e.style.display = 'block';
     }
 }
+
+WebFontConfig = {
+          google: { families: [ 'Abel::latin' ] }
+            };
+              (function() {
+                      var wf = document.createElement('script');
+                          wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+                                '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+                                    wf.type = 'text/javascript';
+                                        wf.async = 'true';
+                                            var s = document.getElementsByTagName('script')[0];
+                                                s.parentNode.insertBefore(wf, s);
+                                                  })(); </script>
