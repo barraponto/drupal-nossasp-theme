ab2d060e5bffd078888a43f94190e870bb212c94
Capi Etheriel <barraponto@gmail.com>
2011-10-26 12:14:32 -0200

    ajustando o botão de ok, posicionando o primeiro dropdown

--- js/nossasp.js
+++ js/nossasp.js
@@ -5,12 +5,12 @@
     // workaround: por algum motivo, esse wrapper vem com um display:block
     $('#edit-circle-location-wrapper, context').attr('style', '');
 
-    $('.vew-0 > label, context').click(function() {
+    $('.vew-0 > label', context).click(function() {
       $('.active', context).removeClass('active').find('.form-text').val('');
       $(this).addClass('active');
-      $(this).siblings('.views-widget').find('input.form-text').addClass('active').focus();
+      $(this).siblings('.views-widget').find('.form-text').addClass('active').focus();
     } );
-    $('.vew-1 > label, context').click(function() {
+    $('.vew-1 > label', context).click(function() {
       $('.active', context).removeClass('active').val('');
       $(this).addClass('active');
       $(this).siblings('.views-widget').find('#edit-circle-location-wrapper').addClass('active').children('.form-text').focus();
