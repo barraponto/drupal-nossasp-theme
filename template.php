614f7ca8a390a68d94dcc078b446212e9c00d46b
Capi Etheriel <barraponto@gmail.com>
2011-10-26 21:48:11 -0200

    acertando os campos do node

--- template.php
+++ template.php
@@ -115,10 +115,8 @@ function nossasp_preprocess_page(&$vars, $hook) {
  * @param $hook
  *   The name of the template being rendered ("node" in this case.)
  */
-/* -- Delete this line if you want to use this function
 function nossasp_preprocess_node(&$vars, $hook) {
-  $vars['sample_variable'] = t('Lorem ipsum.');
-
+  $vars['terms'] = FALSE;
   // Optionally, run node-type-specific preprocess functions, like
   // nossasp_preprocess_node_page() or nossasp_preprocess_node_story().
   $function = __FUNCTION__ . '_' . $vars['node']->type;
@@ -126,7 +124,6 @@ function nossasp_preprocess_node(&$vars, $hook) {
     $function($vars, $hook);
   }
 }
-// */
 
 /**
  * Override or insert variables into the comment templates.
