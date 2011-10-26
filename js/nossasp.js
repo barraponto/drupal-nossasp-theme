a4e63970de61022f78843695c0e723754a90bc03
Capi Etheriel <barraponto@gmail.com>
2011-10-26 10:49:05 -0200

    resetando o valor do campo inativo, ajustando o dropdown do geocoder

--- js/nossasp.js
+++ js/nossasp.js
@@ -6,14 +6,14 @@
     $('#edit-circle-location-wrapper, context').attr('style', '');
 
     $('.vew-0 > label, context').click(function() {
-      $('.active', context).removeClass('active');
+      $('.active', context).removeClass('active').find('.form-text').val('');
       $(this).addClass('active');
-      $(this).siblings('.views-widget').find('input.form-text').addClass('active');
+      $(this).siblings('.views-widget').find('input.form-text').addClass('active').focus();
     } );
     $('.vew-1 > label, context').click(function() {
-      $('.active', context).removeClass('active');
+      $('.active', context).removeClass('active').val('');
       $(this).addClass('active');
-      $(this).siblings('.views-widget').find('#edit-circle-location-wrapper').addClass('active');
+      $(this).siblings('.views-widget').find('#edit-circle-location-wrapper').addClass('active').children('.form-text').focus();
     } );
 
     $('.vew-0 > label').click();
