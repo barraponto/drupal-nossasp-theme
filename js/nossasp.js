e4326e1a04aae94012339dc567f7f315e0120261
Capi Etheriel <barraponto@gmail.com>
2011-10-26 00:30:21 -0200

    turning the js off

--- js/nossasp.js
+++ js/nossasp.js
@@ -1,10 +1,10 @@
 /* função que mostra/esconde o box do filtro "Área de atuação" */
-function area_atuacao(id) {
-  var e = document.getElementById(id);
-  if(e.style.display=='block') {
-    e.style.display = 'none';
-    document.forms['views-exposed-form-nossasp-organizations-map-page-2'].submit();
-  } else {
-    e.style.display = 'block';
-  }
-}
+//function area_atuacao(id) {
+//  var e = document.getElementById(id);
+//  if(e.style.display=='block') {
+//    e.style.display = 'none';
+//    document.forms['views-exposed-form-nossasp-organizations-map-page-2'].submit();
+//  } else {
+//    e.style.display = 'block';
+//  }
+//}
