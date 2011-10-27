b122e8f57909c4feeb2b4ddcc4a117fdda18aaf6
Capi Etheriel <barraponto@gmail.com>
2011-10-27 15:00:32 -0200

    fazendo o sidebar esticar direitinho

--- js/full-sized-map.js
+++ js/full-sized-map.js
@@ -2,15 +2,21 @@
 
   Drupal.behaviors.fullsizedmap = function(context) {
     $map = $('body.front .openlayers-map', context);
+    $header = $('#header', context);
+
     $sidebartop = $('#block-block-2', context);
     $sidebarcontent = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744, #block-block-6', context);
     $sidebarbottom = $('#block-block-3', context);
-    $header = $('#header', context);
+
+    $resultstop = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744 .view-header', context);
+    $resultscontent = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744 .view-content', context);
+    $resultsbottom = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744 .item-list', context);
 
     $(window).resize(
       function(context) {
         $map.height($(window).height() - $header.height() + 'px');
-        $sidebarcontent.height($(window).height() - (2 * $header.height() + $sidebartop.height() + $sidebarbottom.height()) + 'px');
+        $sidebarcontent.height($(window).height() - (1.5 * $header.height() + $sidebartop.height() + $sidebarbottom.height()) + 'px');
+        $resultscontent.height($sidebarcontent.height() - ($resultstop.height() + $resultsbottom.height()));
       } );
 
     $map.addClass('full-sized');
