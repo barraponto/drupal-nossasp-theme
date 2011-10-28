84267c846b62717a143885f003d424a94688485a
Capi Etheriel <barraponto@gmail.com>
2011-10-28 10:16:40 -0200

    novo bloco de cadastre sua organização

--- js/full-sized-map.js
+++ js/full-sized-map.js
@@ -1,12 +1,12 @@
 (function ($) {
 
   Drupal.behaviors.fullsizedmap = function(context) {
-    $map = $('body.front .openlayers-map', context);
+    $map = $('body.front .openlayers-map, .sidebar', context);
     $header = $('#header', context);
 
     $sidebartop = $('#block-block-2', context);
     $sidebarcontent = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744, #block-block-6', context);
-    $sidebarbottom = $('#block-block-3', context);
+    $sidebarbottom = $('#block-nossasp_contexts-0', context);
 
     $resultstop = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744 .view-header', context);
     $resultscontent = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744 .view-content', context);
@@ -14,8 +14,9 @@
 
     $(window).resize(
       function(context) {
-        $map.height($(window).height() - $header.height() + 'px');
-        $sidebarcontent.height($(window).height() - (1.5 * $header.height() + $sidebartop.height() + $sidebarbottom.height()) + 'px');
+        $map.height($(window).height() - $header.height() - 2 + 'px');
+
+        $sidebarcontent.height($(window).height() - ($header.height() + $sidebartop.height() + $sidebarbottom.height()) - 22 + 'px');
         $resultscontent.height($sidebarcontent.height() - ($resultstop.height() + $resultsbottom.height()));
       } );
 
