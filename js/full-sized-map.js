3762213afe8c0762eb8c34e680b5a8ada0f3215b
Capi Etheriel <barraponto@gmail.com>
2011-10-28 13:13:40 -0200

    trocando a barra lateral

--- js/full-sized-map.js
+++ js/full-sized-map.js
@@ -4,8 +4,7 @@
     $map = $('body.front .openlayers-map, .sidebar', context);
     $header = $('#header', context);
 
-    $sidebartop = $('#block-block-2', context);
-    $sidebarcontent = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744, #block-block-6', context);
+    $sidebarcontent = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744, #block-boxes-nossasp_contexts_about', context);
     $sidebarbottom = $('#block-nossasp_contexts-0', context);
 
     $resultstop = $('#block-views-ac1d059bc0aeabb5015c2d28f61ba744 .view-header', context);
@@ -16,7 +15,7 @@
       function(context) {
         $map.height($(window).height() - $header.height() - 2 + 'px');
 
-        $sidebarcontent.height($(window).height() - ($header.height() + $sidebartop.height() + $sidebarbottom.height()) - 22 + 'px');
+        $sidebarcontent.height($(window).height() - ($header.height() + $sidebarbottom.height()) - 2 + 'px');
         $resultscontent.height($sidebarcontent.height() - ($resultstop.height() + $resultsbottom.height()));
       } );
 
