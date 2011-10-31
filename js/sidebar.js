c6c4f9e98c6b39c9ff9094579e93abb52c2e12bd
Murilo Prestes <m4k2005@yahoo.com>
2011-10-31 18:08:23 -0200

    sidebar com condição de exibição

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
   
-	$('#block-boxes-nossasp_contexts_about .title').click(function() {
-		$('#block-boxes-nossasp_contexts_about .content').toggle("slow");
-		$('.content .view-content').toggle("slow");
-		$('.item-list').toggle("slow");
-	});
+	if ($('.view-header').length != 0) {
+    	$('#block-boxes-nossasp_contexts_about .title').click(function() {
+			$('#block-boxes-nossasp_contexts_about .content').toggle("slow");
+			$('.content .view-content').toggle("slow");
+			$('.item-list').toggle("slow");
+		});
 
-	$('.view-header').click(function () {
-		$('#block-boxes-nossasp_contexts_about .content').toggle("slow");
-		$('.content .view-content').toggle("slow");
-		$('.item-list').toggle("slow");
-	});
+		$('.view-header').click(function () {
+			$('#block-boxes-nossasp_contexts_about .content').toggle("slow");
+			$('.content .view-content').toggle("slow");
+			$('.item-list').toggle("slow");
+		});
+  	} else {
+    	$('#block-boxes-nossasp_contexts_about .content').toggle("slow");
+  	}
+
+
 });
